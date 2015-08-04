679454afd8a725e0b2dde37c1d84f5db4753fad4
Xavier Lange <xrlange@gmail.com>
2015-08-03 20:39:53 -0700

    A scamp plugin that boots. woooow

--- output.go
+++ output.go
@@ -1,8 +1,54 @@
 package heka_scamp
 
-import "github.com/gudtech/scamp/scamp"
+import "fmt"
+import "github.com/gudtech/scamp-go/scamp"
 import "github.com/mozilla-services/heka/pipeline"
 
-init(){
-	
+type SCAMPOutputPluginConfig struct {
+	Service string `toml:"service"`
+}
+
+type SCAMPOutputPlugin struct {
+	conf *SCAMPOutputPluginConfig
+	conn *scamp.Connection
+}
+
+func (sop *SCAMPOutputPlugin) ConfigStruct() interface{} {
+		fmt.Println("ConfigStruct")
+	return &SCAMPOutputPluginConfig {
+		Service: ":30101",
+	}
+}
+
+func (sop *SCAMPOutputPlugin) Init(config interface{}) (err error) {
+	sop.conf = config.(*SCAMPOutputPluginConfig)
+
+	fmt.Printf("Connecting to %s\n", sop.conf.Service)
+	sop.conn,err = scamp.Connect(sop.conf.Service)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (sop *SCAMPOutputPlugin) Run(or pipeline.OutputRunner, h pipeline.PluginHelper) (err error){
+	fmt.Errorf("running")
+	for pack := range or.InChan() {
+		fmt.Errorf("got a payload")
+		payload := pack.Message.Payload
+		fmt.Printf("payload: %s", payload)
+	}
+	return
+}
+
+func (sop *SCAMPOutputPlugin) CleanUp() {
+	return
+}
+
+func init(){
+	pipeline.RegisterPlugin("ScampOutput", func() interface{} {
+		fmt.Println("RegisterPlugin/SCAMP")
+		return new(SCAMPOutputPlugin)
+	})
 }
