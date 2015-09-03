e383e67775483cfb760ba90efb090bd5fedd281d
Xavier Lange <xrlange@gmail.com>
2015-09-03 15:39:24 -0700

    Clean up when done

--- docker/build.sh
+++ docker/build.sh
@@ -7,4 +7,6 @@ docker build --rm -t xrlx/heka_scamp_build .
 
 # run the build env CMD for copying deb and installing to minimal image
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -ti xrlx/heka_scamp_build
-# docker rmi $BUILD_IMAGE
+
+# clean up
+docker rmi xrlx/heka_scamp_build
