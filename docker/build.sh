b8dc347a7fa04bbc261ae35d04d44c07c26698b5
Xavier Lange <xrlange@gmail.com>
2015-11-18 13:38:49 -0800

    Documentation clarification

--- docker/build.sh
+++ docker/build.sh
@@ -7,7 +7,7 @@ docker build --no-cache=false --rm -t xrlx/heka_base ../heka
 # prepare the scamp plugin using our heka_base build environment
 docker build --no-cache=true --rm -t xrlx/heka_scamp_build ./docker
 
-# run the build env CMD for copying deb and installing to minimal image
+# copy the .deb in to a fresh build and tag for release
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -ti xrlx/heka_scamp_build
 
 docker tag -f xrlx/heka_scamp gcr.io/retailops-1/heka_scamp
