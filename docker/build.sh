333ecaaeea4eb12c9e4ea99f7680c0768b7a1b96
Xavier Lange <xrlange@gmail.com>
2016-03-14 10:30:39 -0700

    Remove need to push heka-scamp code to server before running docker build

--- docker/build.sh
+++ docker/build.sh
@@ -2,14 +2,14 @@
 set -xe
 
 # assume heka and heka-scamp are siblings in a directory
-# docker build --no-cache=false --rm -t xrlx/heka_base ../heka
+docker build --no-cache=false --rm -t xrlx/heka_base ../heka
 
 # we run most of the heka build one time so it's quick to do our plugin build
 # docker build --rm -t xrlx/heka_base -f docker/Dockerfile.heka .
 
 # prepare the scamp plugin using our heka_base build environment
 # docker build --no-cache=true --rm -t xrlx/heka_scamp_build ./docker
-docker build --no-cache=true --rm -t xrlx/heka_scamp_build ./docker
+docker build --no-cache=true --rm -t xrlx/heka_scamp_build -f ./docker/Dockerfile .
 
 # copy the .deb in to a fresh build and tag for release
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -ti xrlx/heka_scamp_build
